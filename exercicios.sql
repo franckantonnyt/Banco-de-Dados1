582c6304d28b050c9b7008ece8e2943d2dd88954
Franck Antonny <36697614+franckantonnyt@users.noreply.github.com>
2019-06-07 22:34:14 -0300

    Update exercicios.sql

--- exercicios.sql
+++ exercicios.sql
@@ -14,18 +14,33 @@ from Funcionario, Dependente
 where Funcionario.idFunc = Dependente.idFunc and nomeFunc='João B. Silva';
 -- 4 - Selecione o nome dos projetos que o funcionário de nome ‘Frank T. Santos’ trabalha
 select nomeProj
-from Funcionario, Projeto
-where Funcionario.idDepto = Projeto.idDepto and nomeFunc='Frank T. Santos';
+from Funcionario F, Trabalha T,Projeto P
+where nomeFunc='Frank T. Santos' and F.idFunc = T.idFunc and T.idProj = P.idProj;
 -- 5 - Selecione o nome dos funcionários que trabalham em projetos controlados pelo departamento
 -- de nome ‘ Construção’.
 select nomeFunc
-from Funcionario, Departamento
-where Funcionario.idDepto = Departamento.idDepto and nomeDepto='Construçao';
+from Funcionario F, Trabalha T, Projeto P, Departamento D
+where nomeDepto='Construçao' and P.idDepto = D.idDepto and T.idProj = P.idProj and F.idFunc = T.idFunc;
 -- 6 - Selecione o  nome  dos funcionários  supervisionados pelo funcionário  de nome ‘Frank  T.
 -- Santos’.
-select nomeFunc 
+select A.nomeFunc
+from Funcionario F, Funcionario A
+where F.nomeFunc='Frank T. Santos' and F.idFunc = A.idSuperv;
+-- 7 - Selecione o nome e endereço dos funcionários que não tem nenhum dependente.
+select nomeFunc
 from Funcionario
-where idSuperv > all
-	(select idFunc 
-	from Funcionario
-	where nomeFunc='Frank T.Santos');
+where idFunc not in
+	(select idFunc
+  from Dependente);
+-- 8 - Selecione o nome dos funcionários que trabalham no departamento de nome ‘Pesquisa’ ou que
+-- trabalham no projeto de nome ‘N. Benefícios’.
+select nomeFunc
+from Funcionario, Departamento
+where nomeDepto = 'Pesquisa' and Departamento.idDepto = Funcionario.idDepto
+union
+select nomeFunc
+from Funcionario F, Trabalha T, Projeto P
+where nomeProj = 'N. Benefícios' and T.idProj = P.idProj and F.idFunc = T.idFunc;
+-- 9 - Selecione   o   nome   dos   funcionários   que   trabalham   em   algum   projeto   controlado   pelo
+-- departamento cujo gerente é o funcionário de nome ‘Júnia B. Mendes’.
+
