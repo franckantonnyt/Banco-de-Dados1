f69dd731aa00b161b250efb88abb81569ecf2b14
Franck Antonny <36697614+franckantonnyt@users.noreply.github.com>
2019-06-12 20:38:22 -0300

    Update exercicios.sql

--- exercicios.sql
+++ exercicios.sql
@@ -43,4 +43,24 @@ from Funcionario F, Trabalha T, Projeto P
 where nomeProj = 'N. Benefícios' and T.idProj = P.idProj and F.idFunc = T.idFunc;
 -- 9 - Selecione   o   nome   dos   funcionários   que   trabalham   em   algum   projeto   controlado   pelo
 -- departamento cujo gerente é o funcionário de nome ‘Júnia B. Mendes’.
+select nomeFunc
+from Funcionario F, Trabalha T, Projeto P, Departamento D
+where nomeFunc = 'Júnia B. Mendes' and P.idDepto = D.idDepto and T.idProj = P.idProj and F.idFunc = T.idFunc;
+-- 10 - Selecione o nome dos funcionários que trabalham em todos os projetos controlados pelo
+-- departamento cujo gerente é o funcionário de nome ‘Júnia B. Mendes’.
 
+-- 11 Selecione o nome dos funcionários e o nome de seus dependentes. Deve incluir o nome dos
+-- funcionários sem dependentes
+select nomeFunc, nomeDep
+from Funcionario F LEFT OUTER JOIN Dependente D ON (F.idFunc = D.idFunc);
+-- 12 - Selecione a quantidade de funcionários que trabalham no departamento que controla o projeto
+-- de nome ‘ProdZ’.
+select count(*) 
+from Funcionario F, Trabalha T, Projeto P
+where nomeProj = 'ProdZ' and T.idProj = P.idProj and F.idFunc = T.idFunc;
+-- 13 - Selecione o nome dos funcionários e a quantidade de projetos que cada um trabalha mais de 10
+-- horas.
+select F.idFunc, nomeFunc, count(*)
+from Funcionario F, Trabalha T, Projeto P
+where numHoras > 10.00 and T.idProj = P.idProj and F.idFunc = T.idFunc
+group by F.idFunc, nomeFunc;
